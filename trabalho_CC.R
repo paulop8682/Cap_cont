011e8d5c9db4d01e055a2ea29a474fab3deaf802
Paulo Henrick <henrickp31@gmail.com>
2022-02-14 10:33:13 -0300

    atualização 2

--- trabalho_CC.R
+++ trabalho_CC.R
@@ -19,6 +19,49 @@ ggplot(t, aes(year, KOFGI, color = code)) +
   geom_line(size = 1.2) +
   theme_light()
  
+table_kof <- na.omit(KOFGI_2020[,1:4]) %>% 
+  group_by(code, country) %>% summarise(mean(KOFGI))
+
+top_n(table_kof, -5)
+
+AL <- c("ARG", "BRA", "CHL", "CUB", "VEN", "COL", "CRI",
+        "ECU", "GTM", "HND", "HTI", "MEX", "NIC", "PAN",
+        "PRY", "PER", "URY")
+
+func_al <- function(x) {
+  fca <- filter(table_kof, code == x)
+  return(fca)
+}
+
+AL <- data.frame(vari = rbind(func_al("ARG"), func_al("BRA"), func_al("CHL"), func_al("CUB"),
+                        func_al("VEN"), func_al("COL"), func_al("CRI"), func_al("ECU"),
+                        func_al("GTM"), func_al("HND"), func_al("HTI"), func_al("MEX"),
+                        func_al("NIC"), func_al("PAN"), func_al("PRY"), func_al("PER"), 
+                        func_al("URY"),))
+
+names(AL) <- c("Code", "Country", "KOFGI")
+AL$KOFGI <- round(AL$KOFGI, 2)
+
+ggplot(AL, aes(Code, KOFGI)) + 
+  geom_col(fill = "darkred", color = "black") +
+  theme_classic(base_size = 18) + 
+  ggtitle("Índice KOF da América Latina",
+          subtitle = "Média de 1970 à 2016") +
+  xlab("")
+
+
+library(DT)
+datatable(AL)
+
+
+
+
+
+
+
+
+
+
 
 
 
